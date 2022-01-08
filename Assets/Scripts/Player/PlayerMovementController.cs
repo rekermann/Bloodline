6147f0c3625d9c48681ab7a7c9906d3592b2845d
rekermann <rickardekermann@gmail.com>
2022-01-08 19:52:45 +0100

    .

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using Tiles;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class PlayerMovementController : MonoBehaviour
 {
